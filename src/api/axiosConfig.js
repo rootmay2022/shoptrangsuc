4ffce1c12136ad34f3a903f7364a538ff14e4c3b
nguyen khanh hung <nguyenkhanhhung1531@gmail.com>
2026-01-20 10:15:13 +0700

    fix

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,25 +1,21 @@
-// src/api/axiosConfig.js
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080/api', // URL của backend Spring Boot
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-// Interceptor để tự động thêm token vào header của mỗi request
 apiClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
     if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default apiClient;
